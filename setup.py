942a0767b44d8f7c91cc0903c0f4c5ffcd551f40
JavierRodriguezM <71197841+JavierRodriguezM@users.noreply.github.com>
2020-10-14 16:45:39 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/JavierRodriguezM/Tarea3.git",
     packages=setuptools.find_packages(),
+    install_requires = ['argparse'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
