0d6ca8db31be64f44ee81228614430f26e695fc8
JavierRodriguezM <71197841+JavierRodriguezM@users.noreply.github.com>
2020-10-14 16:09:35 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
-from setuptools import setup
 
-setup(
-    name='Tarea3',
-    version='0.0.1',
-    description='Paquete de metodos para la tarea3',
-    url='https://github.com/JavierRodriguezM/Tarea3.git',
-    author='Javier Rodriguez',
-    author_email='javirodriguez97@gmail.com',
-    license='unlicense',
-    packages=['Tarea3'],
-    zip_safe=False
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Tarea3", # Replace with your own username
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="Paquete de metodos para la tarea3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JavierRodriguezM/Tarea3.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
