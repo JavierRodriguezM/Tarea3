f5e6b38d10cdd41cb58a2aeb67543c99d74dd5d5
JavierRodriguezM <71197841+JavierRodriguezM@users.noreply.github.com>
2020-10-14 22:36:51 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,5 @@
-<<<<<<< Updated upstream
-
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="Tarea3", # Replace with your own username
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="Paquete de metodos para la tarea3",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/JavierRodriguezM/Tarea3.git",
-    packages=setuptools.find_packages(),
-    license='MIT'
-    install_requires=['argparse', 'cv2', 'playsound', 'tabulate'],
-    scripts=['imagen.py', 'audio.py', 'texto.py'],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-=======
 setuptools.setup(
     name="Tarea3",
     version="0.0.1",
@@ -56,5 +31,4 @@ setuptools.setup(
         'Intended Audience :: Developers'
     ],
     python_requires='>=3.5',
->>>>>>> Stashed changes
 )
